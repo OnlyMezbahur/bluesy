059af89bd10f42d3ecdb24ed2f8c7c032ef2a45d
Mezbahur <92021313+OnlyMezbahur@users.noreply.github.com>
2022-12-28 17:11:31 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+// (function($){
+// 	'use strict';
+
+
+// // Mobile Menu
+
+// $('.menu-icon i.fa-bars').on('click', function(){
+
+// 	$('.main-menu').slideDown();
+// 	$('.menu-icon i.fa-times').show();
+// 	$(this).hide();
+
+// });
+
+
+// $('.menu-icon i.fa-times').on('click', function(){
+
+// 	$('.main-menu').slideUp();
+// 	$('.menu-icon i.fa-bars').show();
+// 	$(this).hide();
+
+// })
+
+
+
+// $(window).resize(function(){
+// 	var vamos = $(window).width();
+	
+// 	if (vamos > 991) {
+// 		$('.main-menu').removeAttr('style');
+// 	}
+
+
+// });
+
+
+// $('.main-menu i.fa-search').on('click', function(){
+// 	$('.sidebar').animate({
+// 		left: 0
+// 	});
+// })
+
+// $('.sidebar-close i').on('click', function(){
+// 	$('.sidebar').animate({
+// 		left: -250
+// 	})
+// })
+
+
+
+
+// }) (jQuery);
